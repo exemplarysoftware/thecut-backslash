e4ff90d9b0966ed6d0500096b0e10d8db4ed358a
Matt Austin <matt@busara.com.au>
2010-08-23 14:02:27 +0800

    Added object history template.

--- busaraadmin/options.py
+++ busaraadmin/options.py
@@ -5,5 +5,5 @@ class ModelAdmin(admin.ModelAdmin):
     change_form_template = 'busaraadmin/change_form.html'
     change_list_template = 'busaraadmin/change_list.html'
     delete_confirmation_template = 'busaraadmin/delete_confirmation.html'
-    object_history_template = None
+    object_history_template = 'busaraadmin/object_history.html'
 
