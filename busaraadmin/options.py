6ac9219b4d1c5e87d601bc7c88690be45c23a077
Matt Austin <matt@busara.com.au>
2010-08-23 13:57:32 +0800

    Style changes. Templates for admin login/logout and password change.

--- busaraadmin/options.py
+++ busaraadmin/options.py
@@ -4,4 +4,6 @@ from django.contrib import admin
 class ModelAdmin(admin.ModelAdmin):
     change_form_template = 'busaraadmin/change_form.html'
     change_list_template = 'busaraadmin/change_list.html'
+    delete_confirmation_template = 'busaraadmin/delete_confirmation.html'
+    object_history_template = None
 
