98b2a47f0a76459b63505f328df8f9b61a5cab6c
Matt Austin <matt.austin@thecut.net.au>
2011-01-31 13:00:01 +0800

    Updated attach_mediaset decorator and attach_ctas decorator for compatibility with both versions 0.01 and 0.02 of thecut.media/thecut.ctas.

--- decorators.py
+++ decorators.py
@@ -4,7 +4,13 @@ from django.conf import settings
 def attach_call_to_actions(obj):
     obj.call_to_actions = None
     return obj
-if 'ctas' in settings.INSTALLED_APPS:
+if 'thecut.ctas' in settings.INSTALLED_APPS:
+    try:
+        from thecut.ctas.decorators import attach_call_to_actions
+    except ImportError:
+        pass
+# Compatibility for version 0.01 of ctas app.
+elif: 'ctas' in settings.INSTALLED_APPS:
     try:
         from ctas.decorators import attach_call_to_actions
     except ImportError:
@@ -17,6 +23,12 @@ def attach_mediaset(obj):
 if 'thecut.media' in settings.INSTALLED_APPS:
     try:
         from thecut.media.decorators import attach_mediaset
+    except ImportError:
+        pass
+# Compatibility for version 0.01 of media app.
+elif: 'media' in settings.INSTALLED_APPS:
+    try:
+        from media.decorators import attach_mediaset
     except ImportError:
         pass
 
