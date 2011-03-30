d9dbfa493a6f3d1cdcbf6e7447f619b644bb6a15
Matt Austin <matt.austin@thecut.net.au>
2011-03-30 12:36:44 +0800

    Added BACKSLASH_MENU.

---
+++ thecut/backslash/templatetags/backslash_tags.py
@@ -0,0 +1,20 @@
+from django import template
+from django.conf import settings
+
+
+register = template.Library()
+
+@register.inclusion_tag('backslash/_menu.html', takes_context=True)
+def backslash_menu(context):
+    print 'hello'
+    backslash_menu_setting = getattr(settings, 'BACKSLASH_MENU', False)
+    if backslash_menu_setting:
+        module_string = '.'.join(backslash_menu_setting.split('.')[:-1])
+        var_string = backslash_menu_setting.split('.')[-1]
+        module = __import__(module_string, globals(), locals(), [var_string])
+        backslash_menu = getattr(module, var_string)
+    else:
+        backslash_menu = None
+    context.update({'backslash_menu': backslash_menu})
+    return context
+
