2f855606ccefebaae26fd7ab9483cb3650195cb2
Matt Austin <matt.austin@thecut.net.au>
2014-04-17 13:41:14 +0800

    Revert "Attempt at simplifying search_form tag decoration in backslash_list."
    This reverts commit 4ada29eaa07e08bbeecce94c5a0b018faec2d9c4.

--- thecut/backslash/templatetags/backslash_list.py
+++ thecut/backslash/templatetags/backslash_list.py
@@ -1,9 +1,14 @@
 from __future__ import absolute_import, unicode_literals
-from django.contrib.admin.templatetags.admin_list import search_form
+from django.contrib.admin.views.main import SEARCH_VAR
 from django.template import Library
 
 
 register = Library()
 
 
-search_form = register.include_tag(search_form, 'backslash/search_form.html')
+@register.inclusion_tag('backslash/search_form.html')
+def search_form(cl):
+    """Displays a search form for searching the list."""
+    return {'cl': cl,
+            'show_result_count': cl.result_count != cl.full_result_count,
+            'search_var': SEARCH_VAR}
