bc4bf1f06dcadcbb837998d35d559ed1d0606e3e
Sajith Jayaweera <sajith.jayaweera@thecut.net.au>
2014-06-11 17:26:57 +0800

    Add script to toggle 'grid' class on '.change-list .results' when '#view-tigger' is clicked.

--- thecut/backslash/static/backslash/javascripts/change-list-media.js
+++ thecut/backslash/static/backslash/javascripts/change-list-media.js
@@ -1,3 +1,11 @@
 jQuery(document).ready(function($){
 
+    // add grid class on load
+  $('.changelist .results').addClass('grid');
+
+  // toggle grid class
+  $('#view-trigger').on('click', function(){
+    $('.changelist .results').toggleClass('grid');
+  });
+
 });
